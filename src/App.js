a9ae67e36a8f41d1c3102589de2afbcbdc0018a9
Olliemint <furywrld5@gmail.com>
2022-10-30 13:21:13 +0300

    style: styling blogs display

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Header from './components/Header';
+import Article from './components/Article';
 
 function App() {
   return (
     <div className="App">
       <section>
         <Header></Header>
+        <Article></Article>
      </section>
     </div>
   );
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,85 @@
 import React from 'react'
+import "./style.css";
+
+const blogs = [
+  {
+    id: 1,
+    title: "npm audit: Broken by Design",
+    date: "July 7, 2021",
+    time: "• ☕️☕️☕️ 14 min read",
+    description:
+      "Found 99 vulnerabilities (84 moderately irrelevant, 15 highly irrelevant)",
+  },
+  {
+    id: 2,
+    title: "Before You memo()",
+    date: "February 23, 2021",
+    time: "• ☕️ 5 min read",
+    description: "Rendering optimizations that come naturally.",
+  },
+  {
+    id: 3,
+    title: "What Is JavaScript Made Of?",
+    date: "December 20, 2019 ",
+    time: "• ☕️☕️☕️ 13 min read",
+    description: "Getting a closure on JavaScript",
+  },
+  {
+    id: 4,
+    title: "Writing Resilient Components",
+    date: "March 16, 2019",
+    time: "• ☕️☕️☕️☕️☕️ 24 min read",
+    description: "Four principles to set you on the right path.",
+  },
+  {
+    id: 5,
+    title: "A Complete Guide to useEffect",
+    date: "March 9, 2019",
+    time: "• 🍱🍱🍱🍱 49 min read",
+    description: "Effects are a part of your data flow.",
+  },
+  {
+    id: 6,
+    title: "React as a UI Runtime",
+    date: "February 2, 2019",
+    time: "• 🍱🍱🍱 37 min read",
+    description: "An in-depth description of the React programming model.",
+  },
+];
+    
+
 
 const Article = () => {
+    
+
   return (
-    <div>Article</div>
-  )
+    <section className="center1">
+      <div className="bloglist">
+        {blogs.map((blog) => {
+          return <div>
+          <Blog key={blog.id} {...blog}></Blog>
+        </div>  
+        
+        })}
+      </div>
+    </section>
+  );
+}
+
+const Blog = (props) => { 
+  const {title,date,time,description} = props;
+    return (
+      <div className="blog">
+        <h2 className='blog-title'>{ title }</h2>
+        <div className="time">
+          <span>{date}</span>
+          <span>{time}</span>
+        </div>
+        <p>
+          {description}
+        </p>
+      </div>
+    );
 }
 
 export default Article
